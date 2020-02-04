d0c84641940e8a24b00bef15aef18c840052e7d7
Mateusz Woźniak <matisiek11@gmail.com>
2020-02-04 16:25:14 +0100

    :white_check_mark: Added tests

--- src/projects/projects.controller.spec.ts
+++ src/projects/projects.controller.spec.ts
@@ -1,18 +1,29 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ProjectsController } from './projects.controller';
+import {Test, TestingModule} from '@nestjs/testing';
+import {ProjectsController} from './projects.controller';
+import {mockgooseProvider} from '../../test/misc';
+import {getModelToken} from '@nestjs/mongoose';
+import {ProjectSchema} from '../schemas/project.schema';
+import {FunctionsService} from '../functions/functions.service';
+import {ProjectsService} from './projects.service';
+import {FunctionsController} from '../functions/functions.controller';
 
 describe('Projects Controller', () => {
-  let controller: ProjectsController;
+    let controller: ProjectsController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [ProjectsController],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [mockgooseProvider, {
+                provide: getModelToken('Project'),
+                useFactory: async connection => connection.model('Project', ProjectSchema),
+                inject: ['DbConnectionToken'],
+            }, FunctionsService, ProjectsService],
+            controllers: [ProjectsController],
+        }).compile();
 
-    controller = module.get<ProjectsController>(ProjectsController);
-  });
+        controller = module.get<ProjectsController>(ProjectsController);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
 });
--- src/projects/projects.service.spec.ts
+++ src/projects/projects.service.spec.ts
@@ -1,18 +1,28 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ProjectsService } from './projects.service';
+import {Test, TestingModule} from '@nestjs/testing';
+import {ProjectsService} from './projects.service';
+import {mockgooseProvider} from '../../test/misc';
+import {getModelToken} from '@nestjs/mongoose';
+import {ProjectSchema} from '../schemas/project.schema';
+import {FunctionsService} from '../functions/functions.service';
+import {FunctionsController} from '../functions/functions.controller';
 
 describe('ProjectsService', () => {
-  let service: ProjectsService;
+    let service: ProjectsService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ProjectsService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [mockgooseProvider, {
+                provide: getModelToken('Project'),
+                useFactory: async connection => connection.model('Project', ProjectSchema),
+                inject: ['DbConnectionToken'],
+            }, FunctionsService, ProjectsService],
+            controllers: [FunctionsController],
+        }).compile();
 
-    service = module.get<ProjectsService>(ProjectsService);
-  });
+        service = module.get<ProjectsService>(ProjectsService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
