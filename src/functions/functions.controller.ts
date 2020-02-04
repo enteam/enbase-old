ef16f29944a0bb4338388d614979fb176ec1eff8
Mateusz <matisiek11@gmail.com>
2020-02-04 21:19:28 +0100

    :sparkles: Added http functions executing

--- src/functions/functions.controller.ts
+++ src/functions/functions.controller.ts
@@ -1,16 +1,16 @@
-import {Controller, Param, Post, UploadedFile, UseInterceptors} from '@nestjs/common';
+import {All, Controller, Get, Param, Post, Put, Req, Res, UploadedFile, UseInterceptors} from '@nestjs/common';
 import {FunctionsService} from './functions.service';
 import {FileInterceptor} from '@nestjs/platform-express';
 import {ApiConsumes, ApiBody} from '@nestjs/swagger';
 import {UploadFileObject} from '../objects/uploadFile.object';
 import {ProjectsService} from '../projects/projects.service';
 
-@Controller('functions')
+@Controller('')
 export class FunctionsController {
     constructor(private readonly functionsService: FunctionsService, private readonly projectsService: ProjectsService) {
     }
 
-    @Post(':id')
+    @Post('functions/:id')
     @UseInterceptors(FileInterceptor('zip'))
     @ApiConsumes('multipart/form-data')
     @ApiBody({
@@ -20,6 +20,11 @@ export class FunctionsController {
     async deploy(@Param('id') projectId: string, @UploadedFile() zip: { buffer: Buffer }): Promise<void> {
         const project = await this.projectsService.findById(projectId);
         this.functionsService.deploy(project, zip.buffer);
+    }
+
+    @All(':projectName/:functionName')
+    async execute(@Param('projectName') projectName: string, @Param('functionName') functionName: string, @Req() req, @Res() res) {
+        await this.functionsService.execute(projectName, functionName, req, res);
     }
 
 }
--- src/objects/updateProject.object.ts
+++ src/objects/updateProject.object.ts
@@ -2,8 +2,6 @@ import {ApiProperty} from '@nestjs/swagger';
 
 export class UpdateProjectObject {
     @ApiProperty()
-    name: string;
-    @ApiProperty()
     description: string;
     @ApiProperty()
     mongoHost: string;
